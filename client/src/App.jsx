7188265f7ed525a0a966add7e6bc0b3790621c8a
htyagi5 <harshit2310079@akgec.ac.in>
2026-01-23 10:26:16 +0530

    implement React router Dom and use useContext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,43 @@
-import { useState } from 'react'
 import './App.css'
 import  Upload  from './pages/upload'
-import Pouchdb from './pages/pouchdb'
-import OfflineStore from './pages/OfflineStore'
+import { Signup } from './Auth/Signup'
+import { Login } from './Auth/Login'
+import { BrowserRouter,Route,Routes,Navigate } from 'react-router-dom'
+import { useContext } from 'react'
+import AuthContext, { AuthProvider } from './components/Context'
+import Dashboard from './pages/Dashboard'
+import { Toaster } from 'sonner'
+
+export function AppRoutes(){
+  const {isAuthenticated,loading}=useContext(AuthProvider)
+  if (loading) return <div className="p-10 text-center">Loading ShareSphere...</div>;
+  return(
+    <BrowserRouter>
+       <Routes>
+            <Route path='/signup' element={isAuthenticated? <Navigate to="/" replace/> : <Signup/>}/>  //Adding the replace attribute in "Navigate to="/" replace"  is important.
+             It prevents the /login or /signup page from being saved in the browser's back-button history
+            <Route path='/login' element={isAuthenticated? <Navigate to="/" replace/> : <Login/>}/>
+            <Route path="/" element={
+                isAuthenticated ? <Dashboard/> : <Navigate to="/login"/>
+            }/>
+            <Route path='/upload' element={
+              isAuthenticated  ? <Upload/> : <Navigate to="/login"/>
+            }/>
+          <Route path='*' element={<Navigate to="/"/>}/>
+          </Routes>
+        </BrowserRouter>
+  )
+}
 function App() {
   return (
+    <AuthContext>
       <div>
-        <h1 className='text-green-600 text-5xl font-extrabold'>SharePoint</h1>
-        <OfflineStore/>
-        <Upload/>
-        <Pouchdb/>
+        <Toaster richColors closeButton />
+          <AppRoutes/>
       </div>
+    </AuthContext>
   )
 }
 
-export default App
+export default App;
 
