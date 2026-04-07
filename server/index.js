f37fa18618ebcd6869690c46332a5b737102d9b8
Htyagi69 <contacttyagiharshit@gmail.com>
2026-04-07 11:23:28 +0530

    import fixed for s3 bucket index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { S3Client,PutObjectCommand,GetObjectCommand } from '@aws-sdk/client-s3';
+import { S3Client,PutObjectCommand,GetObjectCommand,DeleteObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import fs from 'fs'
 import multer from 'multer';
