48be76af5492880d551cdbdfa7b57199d1a66f43
Htyagi69 <contacttyagiharshit@gmail.com>
2026-04-07 11:13:09 +0530

    Update auth.js

--- server/Middleware/auth.js
+++ server/Middleware/auth.js
@@ -2,8 +2,8 @@ import {getUser} from '../Auth/auth.js'
 
 async function LoggedInUsersOnly(req,res,next){
        const UserId=req.cookies?.uid;
-       console.log("Raw Cookie Header:", req.headers.cookie); 
-       console.log("Parsed Cookies:", req.cookies);
+       // console.log("Raw Cookie Header:", req.headers.cookie); 
+       // console.log("Parsed Cookies:", req.cookies);
        if(!UserId){
            console.log('Cookie not found');
            return res.status(401).json({ message: "Please login again" });
